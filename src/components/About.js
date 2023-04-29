4d6b056f5030bd01062b32e922dd283638237962
dan <dancr.wley@gmail.com>
2023-04-29 01:59:38 -0400

    finish

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id='about'>
+    <h2>About Me</h2>
+    <p>hello, i am a 29 year old fullstack developer interested in visual art, design, and music.</p>
+    <img src={image} alt='I made this' />
+    </div>;
 }
 
 export default About;
